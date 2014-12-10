23557e0d3046804d7bc523f2213522f7aa6bcd6e
MarkusB <markusb81@web.de>
2014-12-10 22:28:12 +0100

    ApplicationFramework: moved bin cleanup scripts out of bin

---
+++ ApplicationFramework/cleanup_bin_win_dbgs.bat
@@ -0,0 +1,7 @@
+REM This file has to be kept up-to-date all the time.
+REM Whenever you add a new dll or exe,
+REM you also need to add a 'delete' for its debug version here.
+
+cd bin
+
+REM del ...
---
+++ ApplicationFramework/cleanup_deploy_bin_win_AppNameVariant.bat
@@ -0,0 +1,3 @@
+REM Cleans up th bin directory for final deployment.
+
+cleanup_bin_win_dbgs.bat
