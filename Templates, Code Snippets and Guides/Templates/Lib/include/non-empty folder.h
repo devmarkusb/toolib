a201fde1382d5b22374c0f12d84135f37fe30075
MarkusB <markusb81@web.de>
2016-01-04 20:28:29 +0100

    -added cmake switch for building without 3rd party dependencies (e.g. without boost) - default is the opposite -math/scale impovements, added calcScaleTickFromTo

--- Templates, Code Snippets and Guides/Templates/Lib/include/non-empty folder.h
+++
@@ -1,9 +0,0 @@
-#ifndef NON_EMPTY_FOLDER_H_ksunxgf738g3
-#define NON_EMPTY_FOLDER_H_ksunxgf738g3
-
-#include "LibNameDEF.h"
-
-
-LIBNAMESHARED_EXPORT void f();
-
-#endif
--- include/Toolib/math/scale.h
+++ include/Toolib/math/scale.h
@@ -14,6 +14,8 @@
 #include <type_traits>
 #include <cmath>
 #include "floating_point.h"
+#include "../narrow.h"
+#include "../assert.h"
 
 
 namespace too
@@ -50,9 +52,9 @@ template <typename T>
 //  requires T > 0
 inline double calcNiceScaleTick(T RangeMinToMax, unsigned long MaxTickCount)
 {
-    assert(MaxTickCount);
-    const double MaxTickCount_ = static_cast<double>(MaxTickCount);
-    const double MinimalTick = static_cast<double>(RangeMinToMax) / MaxTickCount_;
+	TOO_EXPECT(MaxTickCount);
+	const double MaxTickCount_ = narrow<double>(MaxTickCount);
+	const double MinimalTick = narrow<double>(RangeMinToMax) / MaxTickCount_;
     const double magnitude = std::pow(10.0, std::floor(std::log10(MinimalTick)));
     assert(!too::math::almost_equal<T>(magnitude, 0.0));
     const double residual = MinimalTick / magnitude;
@@ -64,6 +66,21 @@ inline double calcNiceScaleTick(T RangeMinToMax, unsigned long MaxTickCount)
         return 2.0 * magnitude;
     else
         return magnitude;
+}
+
+//! Calculates a scale tick range, that contains \param minDataValue and \param maxDataValue at least, the individual ticks being
+//! \param scaleTick apart. \returns a pair of <min, max> tick values (min, max being a certain integer number of \param scaleTick's apart).
+template <typename T>
+//  requires T number
+inline std::pair<double, double> calcScaleTickFromTo(T minDataValue, T maxDataValue, double scaleTick)
+{
+	TOO_EXPECT(!almost_equal(scaleTick, 0.0));
+	TOO_EXPECT(minDataValue <= maxDataValue);
+	const double minIn = narrow<double>(minDataValue);
+	const double maxIn = narrow<double>(maxDataValue);
+	const double minOut = std::floor(minIn / scaleTick) * scaleTick;
+	const double maxOut = std::ceil(maxIn / scaleTick) * scaleTick;
+	return std::make_pair(minOut, maxOut);
 }
 
 }
