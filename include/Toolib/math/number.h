9fc1ff27984e07615172b279217c315bb1dd6aa5
MarkusB <markusb81@web.de>
2016-02-02 18:48:37 +0100

    number.h: added toLeadingZeros

--- include/Toolib/math/number.h
+++ include/Toolib/math/number.h
@@ -11,7 +11,12 @@
 #define NUMBER_H_INCL_f29jh8hnf238hrxz23
 
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <type_traits>
 #include "Toolib/enum_cast.h"
+#include "Toolib/assert.h"
+
 
 namespace too
 {
@@ -42,6 +47,18 @@ template <typename T>
 int sgn(T val)
 {
     return (T(0) < val) - (val < T(0));
+}
+
+template <typename T>
+std::string toLeadingZeros(T x, int digits)
+{
+    static_assert(std::is_integral<T>::value, "");
+    TOO_EXPECT_THROW(x >= T());
+    TOO_EXPECT_THROW(digits >= 0);
+
+    std::stringstream ss;
+    ss << std::setw(digits) << std::setfill('0') << x;
+    return ss.str();
 }
 }
 }
