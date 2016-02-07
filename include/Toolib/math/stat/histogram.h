a873430324fa776b799a90cf6d105f3c3de1c758
MarkusB <markusb81@web.de>
2016-02-07 22:41:32 +0100

    Histogram_percentual: various fixes concerning the percent ranges

--- include/Toolib/math/stat/histogram.h
+++ include/Toolib/math/stat/histogram.h
@@ -16,6 +16,7 @@
 #include "Toolib/math/percent.h"
 #include "Toolib/narrow.h"
 #include "Toolib/PPDEFS.h"
+#include "Toolib/math/floating_point.h"
 
 
 namespace too
@@ -44,10 +45,10 @@ public:
         if (data.empty())
             return;
 
-        const auto minmax = std::minmax(std::begin(data), std::end(data));
+        const auto minmax = std::minmax_element(std::begin(data), std::end(data));
 
         too::math::Map_LinearScale_Interval_to_Interval<Percent> mapData2Percent(
-            std::make_pair(too::narrow_cast<Percent>(*minmax.first), too::narrow_cast<Percent>(*minmax.second)),
+            std::make_pair(Percent(), too::narrow_cast<Percent>(*minmax.second)),
                     std::make_pair(zero_percent, one_hundred_percent));
 
         std::vector<Percent> percentual_data;
@@ -60,21 +61,39 @@ public:
         std::sort(std::begin(percentual_data), std::end(percentual_data));
 
         std::array<size_t, number_of_bars> abs_rates;
+        size_t sum_of_rates = 0;
 
-        auto it = std::begin(percentual_data);
+        auto prev_it = std::begin(percentual_data);
+        auto it = prev_it;
         for (BarCount i = BarCount{}; i < number_of_bars; ++i)
         {
-            it = std::upper_bound(it, std::end(percentual_data), (i + 1) * 10.0);
-            if (it != std::end(percentual_data))
-                abs_rates[i] = std::distance(std::begin(percentual_data), it);
+            it = std::upper_bound(prev_it, std::end(percentual_data), (i + 1) * 10.0);
+            if (it != std::end(percentual_data) && it != prev_it)
+            {
+                abs_rates[i] = std::distance(prev_it, it);
+                sum_of_rates += abs_rates[i];
+            }
             else
                 abs_rates[i] = 0;
+            prev_it = it;
+        }
+        // 100% data entries aren't counted in the previous loop, only smaller ones;
+        // already tried lower_bound and almost<=
+        for (auto it = percentual_data.rbegin(); it != percentual_data.rend(); ++it)
+        {
+            if (too::math::almost_equal(*it, too::math::one_hundred_percent))
+            {
+                ++abs_rates[abs_rates.size() - 1];
+                ++sum_of_rates;
+            }
+            else
+                break;
         }
 
-        const auto minmax_rates = std::minmax(std::begin(abs_rates), std::end(abs_rates));
+        const auto minmax_rates = std::minmax_element(std::begin(abs_rates), std::end(abs_rates));
 
         too::math::Map_LinearScale_Interval_to_Interval<Percent> mapAbsRates2Rel(
-            std::make_pair(*minmax_rates.first, *minmax_rates.second), std::make_pair(zero_percent, one_hundred_percent));
+            std::make_pair(Percent(), sum_of_rates), std::make_pair(zero_percent, one_hundred_percent));
 
         std::transform(std::begin(abs_rates), std::end(abs_rates), std::begin(this->bars), [&](size_t ar)
             {
