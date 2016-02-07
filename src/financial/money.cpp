42c96508a79dc055b52c3622ce4f68812b043769
MarkusB <markusb81@web.de>
2016-02-07 22:40:08 +0100

    Money: <= and >= using approximate equality like ==

--- src/financial/money.cpp
+++ src/financial/money.cpp
@@ -184,9 +184,15 @@ bool operator!=(const Money& lhs, const Money& rhs) { return !operator==(lhs, rh
 
 bool operator>(const Money& lhs, const Money& rhs) { return operator<(rhs, lhs); }
 
-bool operator<=(const Money& lhs, const Money& rhs) { return !operator>(lhs, rhs); }
+bool operator<=(const Money& lhs, const Money& rhs)
+{
+    return operator==(lhs, rhs) || !operator>(lhs, rhs);
+}
 
-bool operator>=(const Money& lhs, const Money& rhs) { return !operator<(lhs, rhs); }
+bool operator>=(const Money& lhs, const Money& rhs)
+{
+    return operator==(lhs, rhs) || !operator<(lhs, rhs);
+}
 
 
 //##########################################################################################################
