93fbf0597e4e351fae6178b9a5bacb86c594ca8c
MarkusB <markusb81@web.de>
2016-10-04 22:11:51 +0200

    added math consts

---
+++ include/Toolib/math/consts.h
@@ -0,0 +1,34 @@
+// Markus Borris, 2016
+// This file is part of Toolib library.
+
+//!
+/**
+*/
+//! \file
+
+#ifndef CONSTS_H_INCL_sjkhnfxgiuexernhgf7834t4
+#define CONSTS_H_INCL_sjkhnfxgiuexernhgf7834t4
+
+#include <cmath>
+#include "Toolib/PPDEFS.h"
+#include "Toolib\std\std_extensions.h"
+
+
+namespace too
+{
+namespace math
+{
+namespace consts
+{
+
+template <typename T>
+constexpr T pi()
+{
+    return std::acos(static_cast<T>(-1));
+}
+
+}
+}
+}
+
+#endif
---
+++ test/math/consts.test.cpp
@@ -0,0 +1,23 @@
+#include "Toolib\math\consts.h"
+#include "gtest/gtest.h"
+#include "Toolib\math\floating_point.h"
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+TEST(math_constsTest, test)
+{
+    double pi = static_cast<double>(M_PI);
+    EXPECT_DOUBLE_EQ(pi, too::math::consts::pi<double>());
+    EXPECT_TRUE(too::math::almost_equal(pi, too::math::consts::pi<double>()));
+
+    float pi_f = static_cast<float>(M_PI);
+    EXPECT_DOUBLE_EQ(pi_f, too::math::consts::pi<float>());
+    EXPECT_TRUE(too::math::almost_equal(pi_f, too::math::consts::pi<float>()));
+
+    long double pi_ld = static_cast<long double>(M_PI);
+    EXPECT_TRUE(too::math::almost_equal(pi_ld, too::math::consts::pi<long double>()));
+}
+
+#undef _USE_MATH_DEFINES
