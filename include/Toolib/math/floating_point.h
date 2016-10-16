191c0a5d07c28382fbfc1bf1ec78effc65ad7ea3
MarkusB <markusb81@web.de>
2016-10-16 21:08:26 +0200

    floating_point: added almost_equal_alltypes

--- include/Toolib/math/floating_point.h
+++ include/Toolib/math/floating_point.h
@@ -9,16 +9,18 @@
 #ifndef FLOATING_POINT_H_INCL_09458t87t5twd2874t57rbn6
 #define FLOATING_POINT_H_INCL_09458t87t5twd2874t57rbn6
 
-#include <limits>
-#include <cmath>
-#include <type_traits>
-#include <algorithm>
+#include "Toolib/assert.h"
+#include "Toolib\ignore_arg.h"
 #include "Toolib/PPDEFS.h"
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <type_traits>
 
 
+// I don't f***ing care restoring original defs for these...
 #undef min
 #undef max
-
 
 
 namespace too
@@ -26,13 +28,32 @@ namespace too
 namespace math
 {
 
+//! \Returns true, if \params x and y are almost equal. Expects \param ulp >= 1.
 template <typename T>
 typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_equal(T x, T y, int ulp = 1)
 {
+    TOO_EXPECT(ulp >= 1);
     return std::abs(x - y) < std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp ||
         std::abs(x - y) < std::numeric_limits<T>::min();
 }
 
+//! Same as almost_equal, but also falls back to '==' if T is an integer type.
+/** Useful if T is already a more general template parameter in your context.*/
+template <typename T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type almost_equal_alltypes(T x, T y, int ulp = 1)
+{
+    return almost_equal(x, y, ulp);
+}
+
+//! Cf. other declaration of almost_equal_alltypes.
+template <typename T>
+typename std::enable_if<std::numeric_limits<T>::is_integer, bool>::type almost_equal_alltypes(T x, T y, int ulp = 1)
+{
+    too::ignore_arg(ulp);
+    return x == y;
+}
+
+//! Rough version of almost_equal, where you can pass a user defined eps(ilon) within which \params x and y are understood approx. equal.
 template <typename T>
 typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type approx_equal(T x, T y, T eps)
 {
--- test/math/floating_pointTest.cpp
+++ test/math/floating_pointTest.cpp
@@ -2,4 +2,14 @@
 #include "gtest/gtest.h"
 
 
-TEST(Math_almost_equal_Test, Basics) { EXPECT_TRUE(too::math::almost_equal(1.0, 1.0)); }
+TEST(Math_almost_equal_Test, test)
+{
+    EXPECT_TRUE(too::math::almost_equal(1.0, 1.0));
+    EXPECT_FALSE(too::math::almost_equal(1.0, 1.0001));
+}
+
+TEST(Math_almost_equal_alltypes_Test, test)
+{
+    EXPECT_TRUE(too::math::almost_equal_alltypes(1, 1)); 
+    EXPECT_FALSE(too::math::almost_equal_alltypes(1, 2));
+}
