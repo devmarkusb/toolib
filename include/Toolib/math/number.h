2bab63e2fb6f9734f42e75de62ac98807a0d03cd
MarkusB <markusb81@web.de>
2016-10-23 17:04:16 +0200

    number: added is_power_of

--- include/Toolib/math/number.h
+++ include/Toolib/math/number.h
@@ -1,4 +1,4 @@
-// Markus Borris, 2011
+// Markus Borris, 2011-16
 // This file is part of Toolib library.
 
 //!
@@ -11,7 +11,10 @@
 #define NUMBER_H_INCL_f29jh8hnf238hrxz23
 
 #include "Toolib/assert.h"
+#include "Toolib/narrow.h"
 #include "Toolib/enum_cast.h"
+#include "Toolib/optional.h"
+#include <cmath>
 #include <iomanip>
 #include <sstream>
 #include <string>
@@ -33,7 +36,7 @@ enum class ENumSys : unsigned char
 template <typename T>
 inline unsigned char getDigitCount(T Number, ENumSys Base = ENumSys::DEC)
 {
-    static_assert(std::is_integral<T>::value, "Only integral numbers are allowed as input");
+    static_assert(std::is_integral<T>::value, "only integral numbers are allowed as input");
     unsigned char count = 0;
     do
     {
@@ -60,7 +63,27 @@ std::string toLeadingZeros(T x, int digits)
     ss << std::setw(digits) << std::setfill('0') << x;
     return ss.str();
 }
+
+//! \Returns the exponent with whom \param x is a power of \param base, if it is an integer one.
+/** Expects parameters to be > 0 and base != 1.
+    Note that the return value can be < 0, e.g. 0.5 is the -1 power of base 2.*/
+template <typename ArithType>
+//  ArithType expected to be is_arithmetic
+too::opt<ArithType> is_power_of(ArithType x, ArithType base)
+{
+    static_assert(std::is_arithmetic<ArithType>::value, "only arithmetic numbers are allowed as input");
+    TOO_EXPECT(x > ArithType{});
+    TOO_EXPECT(base > ArithType{});
+    TOO_EXPECT(base != static_cast<ArithType>(1));
+
+    const long double exp = std::log(x) / std::log(base);
+    long double intpart{};
+    if (std::modf(exp, &intpart) != 0.0L)
+        return {};
+    return too::narrow_cast<ArithType>(intpart);
 }
-}
+
+} // math
+} // too
 
 #endif
