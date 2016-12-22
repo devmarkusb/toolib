dd99c8f3062826fd4d0259de9e8bce0b0bb1797b
MarkusB <markusb81@web.de>
2016-12-22 20:28:58 +0100

    added rudimentary color types

---
+++ include/Toolib/image/color.h
@@ -0,0 +1,64 @@
+// Markus Borris, 2016
+// This file is part of Toolib library.
+
+//!
+/**
+*/
+//! \file
+
+
+#ifndef COLOR_H_sdfjlhjcge5zrxdgh85
+#define COLOR_H_sdfjlhjcge5zrxdgh85
+
+#include <string>
+
+
+namespace too
+{
+namespace img
+{
+class Color_dbl
+{
+public:
+    Color_dbl() = default;
+    //! \params expected to range between 0.0 and 1.0 (inclusive).
+    Color_dbl(double r, double g, double b, double a = 1.0)
+        : r_{r}, g_{g}, b_{b}, a_{a}
+    {}
+
+    double r() const { return r_; }
+    double g() const { return g_; }
+    double b() const { return b_; }
+    double a() const { return a_; }
+
+private:
+    double r_{};
+    double g_{};
+    double b_{};
+    double a_{};
+};
+
+class Color
+{
+public:
+    Color() = default;
+    //! \params expected in range 0..255.
+    Color(int r, int g, int b, int a = 1.0)
+        : r_{r}, g_{g}, b_{b}, a_{a}
+    {}
+
+    int r() const { return r_; }
+    int g() const { return g_; }
+    int b() const { return b_; }
+    int a() const { return a_; }
+
+private:
+    int r_{};
+    int g_{};
+    int b_{};
+    int a_{};
+};
+} // img
+} // too
+
+#endif
