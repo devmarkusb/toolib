e64e9b325d39b5a9c7178b1dac18f895d9662e20
MarkusB <markusb81@web.de>
2017-04-21 00:24:27 +0200

    templates/snippets: major cleanup

--- Templates, Code Snippets and Guides/Templates/Application/cleanup_bin_win_dbgs.bat
+++
@@ -1,9 +0,0 @@
-REM This file has to be kept up-to-date all the time.
-REM Whenever you add a new dll or exe,
-REM you also need to add a 'delete' for its debug version here.
-
-cd bin
-
-REM del ...
-
-cd ..
