d296e3ec5c3bd0ec7914d7e0c33de3fec22de54f
MarkusB <markusb81@web.de>
2017-06-05 20:49:40 +0200

    languages: added native language names (in UTF-8)

--- include/Toolib/string/languages.h
+++ include/Toolib/string/languages.h
@@ -47,9 +47,42 @@ inline const std::vector<LangID_iso639_1>& getAllTheoreticallyPossibleLangIDs()
     return cachedval;
 }
 
-inline std::string getNativeLanguageName(const LangID_iso639_1&)
+inline std::string getNativeLanguageName(const LangID_iso639_1& id)
 {
-    throw too::not_implemented{"getNativeLanguageName"};
+    if (id == auto_systems_language)
+    {
+        // that's not what you might want; please handle the translation for that word yourself
+        // (on library level I can't decide, whether you want 'auto' or 'systems default' or whatever)
+        TOO_ASSERT_THROW(false);
+    }
+    else if (id == "ar")
+        return "\xD8\xA7\xD9\x84\xD8\xB9\xD8\xB1\xD8\xA8\xD9\x8A\xD8\xA9";
+    else if (id == "bn")
+        return "\xE0\xA6\xAC\xE0\xA6\xBE\xE0\xA6\x82\xE0\xA6\xB2\xE0\xA6\xBE";
+    else if (id == "de")
+        return "Deutsch";
+    else if (id == "en")
+        return "English";
+    else if (id == "es")
+        return "Espa\xC3\xB1""ol";
+    else if (id == "fr")
+        return "Fran\xc3\xa7""ais";
+    else if (id == "hi")
+        return "\xE0\xA4\xB9\xE0\xA4\xBF\xE0\xA4\xA8\xE0\xA5\x8D\xE0\xA4\xA6\xE0\xA5\x80";
+    else if (id == "it")
+        return "Italiano";
+    else if (id == "jp")
+        return "\xE6\x97\xA5\xE6\x9C\xAC\xE8\xAA\x9E";
+    else if (id == "ko")
+        return "\xED\x95\x9C\xEA\xB5\xAD\xEC\x96\xB4";
+    else if (id == "pt")
+        return "Portugu\xC3\xAA""s";
+    else if (id == "ru")
+        return "\xD0\xA0\xD1\x83\xD1\x81\xD1\x81\xD0\xBA\xD0\xB8\xD0\xB9";
+    else if (id == "zh")
+        return "\xE4\xB8\xAD\xE6\x96\x87";
+    else
+        throw too::not_implemented{"unsupported or unknonwn ISO 639-1 language code"};
 }
 
 inline std::string getEnglishLanguageName(const LangID_iso639_1& id)
