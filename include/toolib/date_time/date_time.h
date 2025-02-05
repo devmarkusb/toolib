6aadcbfab16055ec4362ebbaf5a43113d9de3030
MarkusB <>
2025-02-05 23:34:16 +0100

    fix build

--- include/toolib/date_time/date_time.h
+++ include/toolib/date_time/date_time.h
@@ -16,9 +16,62 @@ using Months = int;
 //! Years type, represents absolute as well as difference values.
 using Years = int;
 
+UL_PRAGMA_WARNINGS_PUSH
+UL_WARNING_DISABLE_MSVC(4251)
+
+struct TOOLIBSHARED_EXPORT MonthYearDecl {
+    static constexpr Months twelve = 12;
+    static constexpr std::string string_delim{"/"};
+};
+
+UL_PRAGMA_WARNINGS_POP
+
 //! Ensures month to be 1..12 and month and year having the same sign. Only exception: {0, 0} will be mapped to {0, 0}.
 struct TOOLIBSHARED_EXPORT Normalize {
-    static constexpr void do_it(std::pair<Years, Months>& y_m);
+    constexpr void do_it(std::pair<Years, Months>& y_m) {
+        const int month_sgn = ul::math::sgn(y_m.second);
+        const Months month_abs = std::abs(y_m.second);
+        if (month_abs > MonthYearDecl::twelve) {
+            UL_ASSERT(month_sgn);
+            const Years years_correction = month_sgn * (month_abs / MonthYearDecl::twelve);
+            y_m.first += years_correction;
+            const Months rem_months = month_abs % MonthYearDecl::twelve;
+            if (!rem_months) {
+                // We don't want to result in 0 months, because what should be there
+                // between December and January?!
+                // Although MonthYear types could be suitable for absolutes AND differences
+                // normalization can respect the correct absolute format (at least
+                // for input values of months != 0) since for differences it doesn't matter
+                // if it's called (y=2,m=0) or (y=1,m=12), so choose the latter.
+                y_m.second = month_sgn * MonthYearDecl::twelve;
+                month_sgn > 0 ? --y_m.first : ++y_m.first;
+            } else
+                y_m.second = month_sgn * (month_abs % MonthYearDecl::twelve);
+        }
+        const int year_sgn = ul::math::sgn(y_m.first);
+        if (y_m.second == 0 && y_m.first != 0) {
+            UL_ASSERT(year_sgn);
+            if (year_sgn < 0) {
+                y_m.second = -MonthYearDecl::twelve;
+                ++y_m.first;
+            } else {
+                y_m.second = MonthYearDecl::twelve;
+                --y_m.first;
+            }
+        }
+        if (month_sgn == year_sgn)
+            return;
+        if (y_m.second < 0 && y_m.first > 0) {
+            --y_m.first;
+            y_m.second = MonthYearDecl::twelve + y_m.second;
+            return;
+        }
+        if (y_m.second > 0 && y_m.first < 0) {
+            ++y_m.first;
+            y_m.second = -(MonthYearDecl::twelve - y_m.second);
+            return;
+        }
+    }
 };
 
 //! Doesn't normalize.
@@ -36,16 +89,6 @@ using MonthYear = MonthYearBase<>;
 
 //! Works as a difference type only.
 using MonthYearDur = MonthYearBase<DontNormalize>;
-
-UL_PRAGMA_WARNINGS_PUSH
-UL_WARNING_DISABLE_MSVC(4251)
-
-struct TOOLIBSHARED_EXPORT MonthYearDecl {
-    static constexpr Months twelve = 12;
-    static const std::string string_delim;
-};
-
-UL_PRAGMA_WARNINGS_POP
 
 //! Works as an absolute type as well as a difference type.
 template <class NormalizePolicy>
